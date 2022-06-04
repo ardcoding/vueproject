e4d4c31e44b27f30da26080f490c620789bdb792
Ahmet Rüchan Dora <ahmetdoraa@gmail.com>
2022-06-04 10:24:16 +0300

    VueWebSite
    Vuejs
    Vuetify ile oluşturulmuş web sayfası

---
+++ new folder/first/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-app-bar app clipped-left>
+    <v-app-bar-nav-icon/>
+      <v-icon class="mx-4" large>mdi-video</v-icon>    
+      <v-toolbar-title class="align-center">
+        <span class="title">KOD</span>
+      </v-toolbar-title>
+      <v-spacer/>
+      <v-text-field class="mt-7" solo placeholder="Ara...." single-line append-icon="mdi-magnify" color="green"/>
+      <v-spacer/>
+      <v-btn class="ml-0" icon>
+        <v-icon>mdi-apps</v-icon>
+      </v-btn>
+      <v-btn class="ma-2" icon>
+        <v-icon>mdi-bell</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <div>test</div>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ new folder/first/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <hello-world />
+</template>
+
+<script>
+import HelloWorld from "../components/HelloWorld";
+
+export default {
+  nameHome: "Home",
+
+  components: {
+    HelloWorld,
+  },
+};
+</script>
